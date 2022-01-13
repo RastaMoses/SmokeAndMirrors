e39e8f23db3b2ebb3b9b27708d1db678835d3fd0
Leon <leon@nyoucomer.de>
2022-01-13 15:28:52 +0100

    Added Idle and Walking Animation

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,16 +6,27 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    //Configure Params
+    [SerializeField] private Transform _groundCheck;
+
+    //State
+    private bool _isGrounded;
+    bool isMoving;
+    bool isFalling;
+
+    //Cached Component Reference
     Rigidbody2D rb;
     PlayerController playerController;
-    private bool _isGrounded;
+    Animator animator;
 
-    [SerializeField] private Transform _groundCheck;
+
+
 
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         playerController = GetComponent<PlayerController>();
+        animator = GetComponent<Animator>();
     }
     // Start is called before the first frame update
     void Start()
@@ -42,6 +53,14 @@ public class PlayerMovement : MonoBehaviour
         }
 
         float xVelocity = Input.GetAxisRaw("Horizontal") * playerController.speed;
+        if(rb.velocity.x != 0)
+        {
+            isMoving = true;
+        }
+        else
+        {
+            isMoving = false;
+        }
         float yVelocity = rb.velocity.y;
 
         //TODO -maybe- no movement direction change while jumping
@@ -52,13 +71,36 @@ public class PlayerMovement : MonoBehaviour
             if ((Input.GetButtonDown("Jump") || Input.GetButtonDown("X")) && _isGrounded)
             {
                 yVelocity = playerController.jumpForce;
+                animator.SetTrigger("jump");
             }
             //Fall
             if ((Input.GetButtonUp("Jump") || Input.GetButtonUp("X")) && rb.velocity.y > 0)
             {
                 yVelocity -= playerController.jumpForce * 0.3f;
+                
             }
         }
+        
         rb.velocity = new Vector2(xVelocity, yVelocity);
+
+        
+        if (rb.velocity.y < 0.1f && rb.velocity.y > -0.1f && _isGrounded)
+        {
+            isFalling = false;
+            
+        }
+        else
+        {
+            isFalling = true;
+        }
+        Animations();
     }
+
+
+    private void Animations()
+    {
+        animator.SetBool("isMoving", isMoving);
+        animator.SetBool("isFalling",isFalling);
+    }
+
 }
