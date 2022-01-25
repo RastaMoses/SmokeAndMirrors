699cfa7618bf459692af46b0a3837b421eb017a9
Leon <leon@nyoucomer.de>
2022-01-25 19:01:27 +0100

    Added First Stage of Boss Level

--- Assets/Scripts/Lever/MoveByLever.cs
+++ Assets/Scripts/Lever/MoveByLever.cs
@@ -4,32 +4,48 @@ using UnityEngine;
 
 public class MoveByLever : MonoBehaviour
 {
+    //Serialize Params
     [SerializeField] Transform endPoint;
     [SerializeField] bool lerpMovement;
     [SerializeField] float lerpSpeed = 0.5f;
+    [SerializeField] float time = 2f;
 
-    bool alreadyMoved;
+    //State
+    Vector3 originalTransform;
+    Vector3 goalTransform;
+    bool canMove = true;
+    private void Start()
+    {
+        originalTransform = transform.position;
+        
+    }
     void Update()
     {
-        if (alreadyMoved)
-        {
-            return;
-        }
         if (GetComponent<SwitchCondition>().on)
+        {
+            goalTransform = endPoint.position;
+        }
+        if (!GetComponent<SwitchCondition>().on)
+        {
+            goalTransform = originalTransform;
+        }
+        
+        if (canMove)
         {
             if (lerpMovement)
             {
-                transform.position = Vector3.Lerp(transform.position, endPoint.position, lerpSpeed); 
+                transform.position = Vector3.Lerp(transform.position, goalTransform, lerpSpeed * Time.deltaTime); 
             }
             else
             {
-                transform.position = endPoint.position;
-                transform.rotation = endPoint.rotation;
+                transform.position = goalTransform;
             }
-            if (transform == endPoint)
-            {
-                alreadyMoved = true;
-            }
+            
         }
     }
+
+    
+
+    
+
 }
