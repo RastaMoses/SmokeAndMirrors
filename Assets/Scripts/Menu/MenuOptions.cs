de375b33e0adda7379553c4ab54c5ff7ba05b501
Katharina <kahala.h@gmx.de>
2022-02-01 22:56:31 +0100

    fixed UI not turning off in main menu transition to another scene

--- Assets/Scripts/Menu/MenuOptions.cs
+++ Assets/Scripts/Menu/MenuOptions.cs
@@ -9,25 +9,34 @@ public class MenuOptions : MonoBehaviour
     [SerializeField] private GameObject _optionScreen;
     [SerializeField] private GameObject _mainMenuScreen;
 
+    [SerializeField] private GameObject _exitTriggerZone;
     [SerializeField] private GameObject _optionTriggerZone;
+    [SerializeField] private GameObject _enterTriggerZone;
+
+    private void ActivateMenuOptions(bool activate)
+    {
+        _exitTriggerZone.gameObject.SetActive(activate);
+        _optionTriggerZone.gameObject.SetActive(activate);
+        _enterTriggerZone.gameObject.SetActive(activate);
+    }
 
     public void ExitGame()
     {
         Debug.Log("Exit");
-        SceneLoader.QuitGame();
-        #if UNITY_EDITOR
-         UnityEditor.EditorApplication.isPlaying = false;
-        #endif
+        ActivateMenuOptions(false);
+        StartCoroutine(FindObjectOfType<Game>().QuitGame());
     }
 
     public void EnterGame()
     {
-        StartCoroutine(FindObjectOfType<Game>().PlayCurtainAndLevel(Mathf.Max(PlayerPrefs.GetInt("HighestLevel"), 1)));
         Debug.Log("Enter");
+        ActivateMenuOptions(false);
+        StartCoroutine(FindObjectOfType<Game>().PlayCurtainAndLevel(Mathf.Max(PlayerPrefs.GetInt("HighestLevel"), 1)));
     }
 
     public void NewGame()
     {
+        ActivateMenuOptions(false);
         PlayerPrefs.SetInt("HighestLevel", 1);
         // put 1 here if level build indxes are in sequences and 0 is main menu
         StartCoroutine(FindObjectOfType<Game>().PlayCurtainAndLevel(1));
